9488f69887223b59ee2a6e58533185199f7219d0
Bogdan Buga <bogdandrei04@gmail.com>
2024-11-20 17:54:32 +0200

    First commit

---
+++ src/main/java/com/bogdan/storemanagement_inghubs/StoremanagementInghubsApplication.java
@@ -0,0 +1,13 @@
+package com.bogdan.storemanagement_inghubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoremanagementInghubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoremanagementInghubsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bogdan/storemanagement_inghubs/StoremanagementInghubsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogdan.storemanagement_inghubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoremanagementInghubsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
